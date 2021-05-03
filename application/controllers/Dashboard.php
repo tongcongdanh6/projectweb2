bbf3f2e72209de9ce8fcac15b9d85b8240077877
unknown <tong.congdanh@gmail.com>
2021-05-03 21:50:43 +0700

    add Dashboard

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,20 @@
+<?php
+class Dashboard extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library("session");
+        // Kiểm tra đăng nhập
+        if (!$this->session->has_userdata('logged_in')) {
+            redirect("login");
+        }
+    }
+
+    public function index() {
+
+    }
+
+    public function test() {
+        echo "test";
+    }
+}
