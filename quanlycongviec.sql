01bc5cafec6fbad686c23e20938520ae26b8d1a9
unknown <tong.congdanh@gmail.com>
2021-05-04 21:31:43 +0700

    update db

--- quanlycongviec.sql
+++ quanlycongviec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 04, 2021 at 12:34 PM
+-- Generation Time: May 04, 2021 at 04:30 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -39,7 +39,30 @@ CREATE TABLE `department` (
 
 INSERT INTO `department` (`id`, `slug`, `name`) VALUES
 (1, 'phong-kinh-doanh', 'Phòng kinh doanh'),
-(2, 'phong-it', 'Phòng IT');
+(2, 'phong-it', 'Phòng IT'),
+(3, 'phong-nhan-su', 'Phòng nhân sự'),
+(4, 'phong-ke-toan', 'Phòng kế toán');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `position`
+--
+
+CREATE TABLE `position` (
+  `id` int(11) NOT NULL,
+  `slug` varchar(50) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `position`
+--
+
+INSERT INTO `position` (`id`, `slug`, `name`) VALUES
+(1, 'truong-phong', 'Trưởng phòng'),
+(2, 'pho-phong', 'Phó phòng'),
+(3, 'nhan-vien', 'Nhân viên');
 
 -- --------------------------------------------------------
 
@@ -52,7 +75,8 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
   `fullname` varchar(100) NOT NULL,
-  `role` int(1) NOT NULL,
+  `role` int(1) NOT NULL DEFAULT 2,
+  `position` int(1) NOT NULL DEFAULT 3,
   `department` int(1) NOT NULL,
   `created_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -61,8 +85,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `fullname`, `role`, `department`, `created_at`) VALUES
-(3, 'tong.congdanh@gmail.com', '46c15b98701216b284bc64e7b16d01abdaa4a847ecda894521cbc4cf59a6d73a488b295361823cbb003977064a3bdf037c16cd57f1fa5894c9412869f035a5b7rJm6fXbjBshFg7pkM3ZTf25CFiPt4bl8wB25QIv7rYM=', 'Tống Công Danh', 1, 2, '2021-05-03 05:15:23');
+INSERT INTO `users` (`id`, `email`, `password`, `fullname`, `role`, `position`, `department`, `created_at`) VALUES
+(3, 'tong.congdanh@gmail.com', '46c15b98701216b284bc64e7b16d01abdaa4a847ecda894521cbc4cf59a6d73a488b295361823cbb003977064a3bdf037c16cd57f1fa5894c9412869f035a5b7rJm6fXbjBshFg7pkM3ZTf25CFiPt4bl8wB25QIv7rYM=', 'Tống Công Danh', 1, 1, 2, '2021-05-03 05:15:23'),
+(4, 'romeo.juliet44@yahoo.com', 'de393f5f334e21f623d86b4c8215013b8d80a2776ae18dda5e08753b3a262faa2873fccb1b3edf1a3de0ca3eba4a3fa3816feb16d9b6acc87a786d33bb301307R94SOZw+MqxmIn4waRH8Diq/gH9PVf3uo6TxWHsMl80=', 'Nguyễn Văn A', 2, 3, 3, '2021-05-04 04:06:14');
 
 --
 -- Indexes for dumped tables
@@ -72,6 +97,12 @@ INSERT INTO `users` (`id`, `email`, `password`, `fullname`, `role`, `department`
 -- Indexes for table `department`
 --
 ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `position`
+--
+ALTER TABLE `position`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -88,13 +119,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `position`
+--
+ALTER TABLE `position`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
